7623dbfc23d44fb4bec4133d19ebd6d3fc61a396
oyamasiphula <oyama@projectcodex.co>
2016-05-24 13:48:37 +0200

    physically used for loops to read file

--- readfile.js
+++ readfile.js
@@ -16,15 +16,3 @@ for (var i = 0; i < lines.length; i++) {
     console.log(people);
     return people
 }
-
-var path = require('path');
-var fs = require('fs');
- 
-var filePath = path.join(__dirname, 'Data', 'highrise-sample-people.csv');
- 
-fs.readFile(filePath, 'utf-8', function (err, data) {
-    if (err) {
-        return console.log("Unable to read file " + err);
-    }
-    console.log(data + '\n');
-});
