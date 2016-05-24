1995d9021cf744aa847feeacce8b3459f87b5624
oyamasiphula <oyama@projectcodex.co>
2016-05-24 13:44:12 +0200

    note that:You're not supposed to write synchronous javascript, so never use it, but if you want to see the synchronous version of reading a file in node.

--- jscodeSyncNodejsV.js
+++ jscodeSyncNodejsV.js
@@ -2,8 +2,11 @@
 // so never use this, but if you want to see the synchronous version of reading a file in node:
 // ?
 
+
 var path = require('path');
+
 var fs = require('fs');
+
 var filePath = path.join(__dirname, 'Data', 'highrise-sample-people.csv');
 var file = fs.readFileSync(filePath, 'utf-8');
 console.log(file);
